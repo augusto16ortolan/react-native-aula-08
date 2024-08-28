5580281da78081c46a75e4cf738cbf378d466e6f
Augusto <augustoortolan02@gmail.com>
2024-08-28 20:48:11 -0300

    Ajustes

--- src/pages/DetalhesPessoa.js
+++ src/pages/DetalhesPessoa.js
@@ -1,11 +1,12 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
 
 export default function DetalhesPessoa({ navigation, route }) {
-  const { name, dob } = route.params.person.item;
+  const { name, dob, picture } = route.params.person.item;
 
   return (
     <View styles={styles.container}>
+      <Image source={{ uri: picture.large }} height={200} width={200} />
       <Text style={styles.text}>{name.first}</Text>
       <Text style={styles.text}>{dob.age}</Text>
     </View>
